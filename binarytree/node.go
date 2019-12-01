5c8cbd7f786837bd87dc02dd3facf815831702bc
Michael Angelo Novicio <michael.angelo.novicio@gmail.com>
2019-12-02 01:17:25 +1100

    Add getlvel and gethop

--- binarytree/node.go
+++ binarytree/node.go
@@ -51,6 +51,36 @@ func (n *Node) printBFS(w io.Writer) {
 	}
 }
 
+type NodeWithLevel struct {
+	node  *Node
+	level int
+}
+
+func (n *Node) GetLevel(val int) int {
+	queue := list.New()
+
+	queue.PushBack(&NodeWithLevel{node: n, level: 1})
+
+	for queue.Len() > 0 {
+		element := queue.Front()
+		nodeWithLevel := queue.Remove(element).(*NodeWithLevel)
+
+		if nodeWithLevel.node.Value == val {
+			return nodeWithLevel.level
+		}
+
+		if nodeWithLevel.node.Left != nil {
+			queue.PushBack(&NodeWithLevel{node: nodeWithLevel.node.Left, level: nodeWithLevel.level + 1})
+		}
+
+		if nodeWithLevel.node.Right != nil {
+			queue.PushBack(&NodeWithLevel{node: nodeWithLevel.node.Right, level: nodeWithLevel.level + 1})
+		}
+	}
+
+	return 0
+}
+
 func (n *Node) printDFS(w io.Writer) {
 	fmt.Fprintf(w, "%d ", n.Value)
 
@@ -61,4 +91,43 @@ func (n *Node) printDFS(w io.Writer) {
 	if n.Right != nil {
 		n.Right.printDFS(w)
 	}
+}
+
+func (n *Node) GetHopDFS(val int) int {
+	hop, found := n.GetTraversalCountDFS(val, 0)
+	if found {
+		return hop
+	}
+
+	return 0
+}
+
+func (n *Node) GetTraversalCountDFS(val, hop int) (int, bool) {
+	// fmt.Println("node is now:", n)
+	// fmt.Println("hop is now:", hop)
+
+	if n.Value == val {
+		return hop + 1, true
+	}
+
+	found := false
+	if n.Left != nil {
+		// fmt.Println("checking left")
+		hop, found = n.Left.GetTraversalCountDFS(val, hop+1)
+		// hop = ihop
+		if found {
+			return hop, found
+		}
+	}
+
+	if n.Right != nil {
+		// fmt.Println("checking right")
+		hop, found = n.Right.GetTraversalCountDFS(val, hop+1)
+		// hop = ihop
+		if found {
+			return hop, found
+		}
+	}
+
+	return hop, false
 }
--- binarytree/node_test.go
+++ binarytree/node_test.go
@@ -7,34 +7,35 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_PrintBFS(t *testing.T) {
-	tree := &Node{
-		Value: 5,
+var tree = &Node{
+	Value: 5,
+	Left: &Node{
+		Value: 8,
 		Left: &Node{
-			Value: 8,
-			Left: &Node{
-				Value: 6,
-			},
-			Right: &Node{
-				Value: 2,
-				Left: &Node{
-					Value: 7,
-				},
-				Right: &Node{
-					Value: 9,
-				},
-			},
+			Value: 6,
 		},
 		Right: &Node{
-			Value: 3,
+			Value: 2,
 			Left: &Node{
-				Value: 4,
+				Value: 7,
 			},
 			Right: &Node{
-				Value: 1,
+				Value: 9,
 			},
 		},
-	}
+	},
+	Right: &Node{
+		Value: 3,
+		Left: &Node{
+			Value: 4,
+		},
+		Right: &Node{
+			Value: 1,
+		},
+	},
+}
+
+func Test_PrintBFS(t *testing.T) {
 
 	buf := bytes.NewBufferString("")
 	tree.printBFS(buf)
@@ -42,37 +43,34 @@ func Test_PrintBFS(t *testing.T) {
 	assert.EqualValues(t, "5 8 3 6 2 4 1 7 9 ", buf.String())
 }
 
-func Test_PrintDFS(t *testing.T) {
-	tree := &Node{
-		Value: 5,
-		Left: &Node{
-			Value: 8,
-			Left: &Node{
-				Value: 6,
-			},
-			Right: &Node{
-				Value: 2,
-				Left: &Node{
-					Value: 7,
-				},
-				Right: &Node{
-					Value: 9,
-				},
-			},
-		},
-		Right: &Node{
-			Value: 3,
-			Left: &Node{
-				Value: 4,
-			},
-			Right: &Node{
-				Value: 1,
-			},
-		},
-	}
+func Test_GetLevel(t *testing.T) {
+	actual := tree.GetLevel(4)
+	assert.Equal(t, 3, actual)
 
+	actual = tree.GetLevel(7)
+	assert.Equal(t, 4, actual)
+
+	actual = tree.GetLevel(10)
+	assert.Equal(t, 0, actual)
+}
+
+func Test_PrintDFS(t *testing.T) {
 	buf := bytes.NewBufferString("")
 	tree.printDFS(buf)
 
 	assert.EqualValues(t, "5 8 6 2 7 9 3 4 1 ", buf.String())
+}
+
+func Test_GetHopDFS(t *testing.T) {
+	actual := tree.GetHopDFS(5)
+	assert.Equal(t, 1, actual)
+
+	actual = tree.GetHopDFS(7)
+	assert.Equal(t, 5, actual)
+
+	actual = tree.GetHopDFS(2)
+	assert.Equal(t, 4, actual)
+
+	actual = tree.GetHopDFS(10)
+	assert.Equal(t, 0, actual)
 }
